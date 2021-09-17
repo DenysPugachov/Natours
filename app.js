03b0729b9786f07a56e3120874b9a18e8fe0b9ea
Denys <pugachovdenys@gmail.com>
2021-09-17 12:35:16 +0300

    limit number of req. pro hour with express-rate-limit package

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-//Express related staff
 const express = require("express")
 const morgan = require("morgan")
+const rateLimit = require("express-rate-limit")
 
 const globalErrorHandler = require("./controllers/errorController")
 const AppError = require("./utils/appError")
@@ -9,19 +9,25 @@ const userRouter = require("./routes/userRoutes")
 
 const app = express()
 
-//Middleware
-// console.log(process.env.NODE_ENV)
+// Global Middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev")) // HTTP request logger
 }
+
+// limit requests allowing to certain amount of time from one IP
+const limiter = rateLimit({
+  max: 100, // allow 100 requests from same IP
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: "Too many requests form this IP, please try again in an hour.",
+})
+// Limit access to "/api" route
+app.use("/api", limiter)
 
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`)) //serves static files
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
-  // get access to headers
-  // console.log("\n ======== ================ headers", req.headers)
   next()
 })
 
