f6115de323bd4c3714615fb7d32fb61a8d6aa6de
Denys <pugachovdenys@gmail.com>
2021-09-22 09:51:01 +0300

    157 add nested route tour/review

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const tourController = require("../controllers/tourController")
 const authController = require("../controllers/authController")
+const reviewController = require("../controllers/reviewController")
 
 const router = express.Router()
 
@@ -24,6 +25,18 @@ router
     authController.protect,
     authController.restrictTo("admin", "lead-guide"),
     tourController.deleteTour,
+  )
+
+// POST /tour/id2334/reviews => nested route
+// GET /tour/id2334/reviews => nested route
+// GET /tour/id2334/reviews/id111
+
+router
+  .route("/:tourId/reviews")
+  .post(
+    authController.protect,
+    authController.restrictTo("user"),
+    reviewController.createReview,
   )
 
 module.exports = router
