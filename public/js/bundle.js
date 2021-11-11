76c31fc3c86722a9219433b71460a00432fa28b3
Denys <pugachovdenys@gmail.com>
2021-11-11 22:14:59 +0200

    add gitignore

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9228,7 +9228,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54087" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62167" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
